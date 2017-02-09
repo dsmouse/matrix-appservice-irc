7d91eda4292e8aea37fd0f2a2356240d3431a028
Pierre Jaury <pierre@jaury.eu>
2017-02-09 21:56:38 +0100

    Actually add IP addresses

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ ! -z $IP_BASE ]
 then
   for ADDRESS in $(sipcalc -6 "$IP_BASE/$IP_PREFIX" -S 128 | egrep -o '[0-9a-f:]{39}' | uniq)
   do
-    echo ip a a $ADDRESS/128 dev $IP_IFACE
+    ip a a $ADDRESS/128 dev $IP_IFACE
   done
 
 fi
